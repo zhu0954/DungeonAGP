9dedb48cbb5c4519733a9f0640eda35c88835a7a
Zhu0099 <jonny.lim09@gmail.com>
2024-10-28 15:06:49 +1100

    added pathfinding and weapon pickup

--- Source/AGP/Pickups/PickupManagerSubsystem.cpp
+++ Source/AGP/Pickups/PickupManagerSubsystem.cpp
@@ -1,65 +1,77 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
 #include "PickupManagerSubsystem.h"
-
 #include "WeaponPickup.h"
 #include "AGP/AGPGameInstance.h"
 #include "AGP/Pathfinding/PathfindingSubsystem.h"
+#include "Kismet/GameplayStatics.h"
+#include "Engine/World.h"
 
-void UPickupManagerSubsystem::Tick(float DeltaTime)
+void UPickupManagerSubsystem::Initialize(FSubsystemCollectionBase& Collection)
 {
-	Super::Tick(DeltaTime);
+    Super::Initialize(Collection);
 
-	// We don't want this pickup manager to do any spawning if it isn't
-	// on the server.
-	// A value < NM_Client is any type of server. So if it is >=
-	// to NM_Client or == NM_Client then we know it is the client
-	// and we don't want to spawn.
-	if (GetWorld()->GetNetMode() >= NM_Client)
-	{
-		return;
-	}
-
-	if (PossibleSpawnLocations.IsEmpty())
-	{
-		PopulateSpawnLocations();
-	}
-
-	TimeSinceLastSpawn += DeltaTime;
-	if (TimeSinceLastSpawn >= PickupSpawnRate)
-	{
-		SpawnWeaponPickup();
-		TimeSinceLastSpawn = 0.0f;
-	}
-}
-
-void UPickupManagerSubsystem::SpawnWeaponPickup()
-{
-	if (PossibleSpawnLocations.IsEmpty())
-	{
-		UE_LOG(LogTemp, Error, TEXT("Unable to spawn weapon pickup."))
-		return;
-	}
-	
-	if (const UAGPGameInstance* GameInstance =
-		GetWorld()->GetGameInstance<UAGPGameInstance>())
-	{
-		FVector SpawnPosition =
-			PossibleSpawnLocations[FMath::RandRange(0, PossibleSpawnLocations.Num()-1)];
-		SpawnPosition.Z += 50.0f;
-		GetWorld()->SpawnActor<AWeaponPickup>(
-			GameInstance->GetWeaponPickupClass(), SpawnPosition, FRotator::ZeroRotator);
-
-		//UE_LOG(LogTemp, Display, TEXT("Weapon Pickup Spawned"))
-	}
+    PopulateSpawnLocations();
+    SpawnWeapons();
 }
 
 void UPickupManagerSubsystem::PopulateSpawnLocations()
 {
-	PossibleSpawnLocations.Empty();
-	if (const UPathfindingSubsystem* PathfindingSubsystem = GetWorld()->GetSubsystem<UPathfindingSubsystem>())
-	{
-		PossibleSpawnLocations = PathfindingSubsystem->GetWaypointPositions();
-	}
+    AllRoomSpawnLocations.Empty();
+    UsedSpawnLocations.Empty();
+
+    if (const UPathfindingSubsystem* PathfindingSubsystem = GetWorld()->GetSubsystem<UPathfindingSubsystem>())
+    {
+        TArray<FVector> Waypoints = PathfindingSubsystem->GetWaypointPositions();
+        UE_LOG(LogTemp, Display, TEXT("Total waypoints found: %d"), Waypoints.Num());
+
+        for (const FVector& Waypoint : Waypoints)
+        {
+            // Here you may want to add any logic to filter specific waypoints or adjust their position
+            AllRoomSpawnLocations.Add(Waypoint);
+            UE_LOG(LogTemp, Display, TEXT("Added possible spawn location: %s"), *Waypoint.ToString());
+        }
+    }
+
+    UE_LOG(LogTemp, Display, TEXT("Total possible room spawn locations: %d"), AllRoomSpawnLocations.Num());
+}
+
+void UPickupManagerSubsystem::SpawnWeapons()
+{
+    if (AllRoomSpawnLocations.IsEmpty())
+    {
+        
+        return;
+    }
+
+    if (const UAGPGameInstance* GameInstance = GetWorld()->GetGameInstance<UAGPGameInstance>())
+    {
+        // Loop through each room spawn location and spawn a weapon if it hasn't been used
+        for (const FVector& SpawnLocation : AllRoomSpawnLocations)
+        {
+            if (UsedSpawnLocations.Contains(SpawnLocation))
+            {
+                UE_LOG(LogTemp, Display, TEXT("Location already used for spawning: %s"), *SpawnLocation.ToString());
+                continue;
+            }
+
+            FVector AdjustedSpawnLocation = SpawnLocation;
+            AdjustedSpawnLocation.Z += 50.0f;  // Adjust height to prevent clipping
+
+            AWeaponPickup* SpawnedPickup = GetWorld()->SpawnActor<AWeaponPickup>(
+                GameInstance->GetWeaponPickupClass(), AdjustedSpawnLocation, FRotator::ZeroRotator);
+
+            if (SpawnedPickup)
+            {
+                UE_LOG(LogTemp, Display, TEXT("Successfully spawned weapon pickup at: %s"), *SpawnedPickup->GetActorLocation().ToString());
+                UsedSpawnLocations.Add(SpawnLocation);  // Mark this location as used
+            }
+            else
+            {
+                UE_LOG(LogTemp, Warning, TEXT("Failed to spawn weapon pickup at location: %s"), *AdjustedSpawnLocation.ToString());
+            }
+        }
+    }
+    else
+    {
+        UE_LOG(LogTemp, Error, TEXT("No GameInstance found. Unable to spawn weapon pickups."));
+    }
 }
--- Source/AGP/Pickups/PickupManagerSubsystem.h
+++ Source/AGP/Pickups/PickupManagerSubsystem.h
@@ -1,5 +1,3 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #pragma once
 
 #include "CoreMinimal.h"
@@ -7,34 +5,19 @@
 #include "PickupManagerSubsystem.generated.h"
 
 class AWeaponPickup;
-/**
- * 
- */
+
 UCLASS()
-class AGP_API UPickupManagerSubsystem : public UTickableWorldSubsystem
+class AGP_API UPickupManagerSubsystem : public UWorldSubsystem
 {
 	GENERATED_BODY()
+
 public:
-
-	virtual TStatId GetStatId() const override
-	{
-		return TStatId();
-	}
-
-protected:
-	
-	/**
-	 * The world locations of all possible locations that a pickup can spawn.
-	 */
-	TArray<FVector> PossibleSpawnLocations;
-	float PickupSpawnRate = 5.0f;
-	float TimeSinceLastSpawn = 0.0f;
-	
-	virtual void Tick(float DeltaTime) override;
+	virtual void Initialize(FSubsystemCollectionBase& Collection) override;
 
 private:
-
-	void SpawnWeaponPickup();
 	void PopulateSpawnLocations();
-	
+	void SpawnWeapons();
+
+	TArray<FVector> AllRoomSpawnLocations;     // All possible spawn locations in rooms
+	TArray<FVector> UsedSpawnLocations;        // Tracks spawn locations that have been used for weapon spawns
 };
