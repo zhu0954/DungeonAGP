2c298c8e1db002b07ffcc74ab5bff5654595dd09
Zhu0099 <jonny.lim09@gmail.com>
2024-11-02 21:22:54 +1100

    made it so only the character can pick up the weapon

--- Source/AGP/Pickups/WeaponPickup.cpp
+++ Source/AGP/Pickups/WeaponPickup.cpp
@@ -18,15 +18,12 @@ void AWeaponPickup::BeginPlay()
 }
 
 void AWeaponPickup::OnPickupOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
-                                    UPrimitiveComponent* OtherComponent, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& HitInfo)
+									UPrimitiveComponent* OtherComponent, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& HitInfo)
 {
-	//Super::OnPickupOverlap(OverlappedComponent, OtherActor, OtherComponent, OtherBodyIndex, bFromSweep, HitInfo);
-	// UE_LOG(LogTemp, Display, TEXT("Overlap event occurred on WeaponPickup"))
-
-	if (ABaseCharacter* Player = Cast<ABaseCharacter>(OtherActor))
+	if (APlayerCharacter* Player = Cast<APlayerCharacter>(OtherActor)) // Check if the overlapping actor is the PlayerCharacter
 	{
 		Player->EquipWeapon(true, WeaponStats);
-		Destroy();
+		Destroy(); // Destroy the weapon pickup after it's picked up
 	}
 }
 
